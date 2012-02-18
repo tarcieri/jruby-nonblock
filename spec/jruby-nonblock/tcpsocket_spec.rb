59149add20dab18df829911c6731dd91336b92dc
Tony Arcieri <tony.arcieri@gmail.com>
2012-02-18 15:56:45 -0800

    Specs for read_nonblock

--- spec/jruby-nonblock/tcpsocket_spec.rb
+++ spec/jruby-nonblock/tcpsocket_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe TCPSocket do
+  describe "#read_nonblock" do
+    it "reads if the operation won't block" do
+      setup_tcp do |client, peer|
+        peer << "OHAI"
+        client.read_nonblock(4).should == "OHAI"
+      end
+    end
+
+    it "raises Errno::EWOULDBLOCK if the operation would block" do
+      setup_tcp do |client, peer|
+        expect { client.read_nonblock(4) }.to raise_exception(Errno::EWOULDBLOCK)
+      end
+    end
+  end
+
   describe '#write_nonblock' do
     let(:payload) { 'JUNK IN THE TUBES' }
 
@@ -18,19 +33,19 @@ describe TCPSocket do
         end.to raise_exception(Errno::EWOULDBLOCK)
       end
     end
+  end
 
-    def setup_tcp(&block)
-      addr, tcp_port = '127.0.0.1', rand(10000) + 10000
+  def setup_tcp(&block)
+    addr, tcp_port = '127.0.0.1', rand(10000) + 10000
 
-      server = TCPServer.new(addr, tcp_port)
-      client = TCPSocket.new(addr, tcp_port)
-      peer   = server.accept
+    server = TCPServer.new(addr, tcp_port)
+    client = TCPSocket.new(addr, tcp_port)
+    peer   = server.accept
 
-      block.call(client, peer)
+    block.call(client, peer)
 
-      client.close
-      server.close
-      peer.close
-    end
+    client.close
+    server.close
+    peer.close
   end
 end
